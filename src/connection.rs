030f251abc2911edec349ced5cd3cb705183de47
Andrés Villalobos <andresalvivar@gmail.com>
2021-06-14 21:46:05 -0600

    received and send are byte vecs

--- src/connection.rs
+++ src/connection.rs
@@ -1,13 +1,11 @@
-use std::io::{Read, Write};
-
 use mio::{net::TcpStream, Token};
 
 pub struct Connection {
     pub token: Token,
     pub socket: TcpStream,
     pub is_open: bool,
-    received: String,
-    pub to_send: String,
+    pub received: Vec<u8>,
+    pub to_send: Vec<u8>,
 }
 
 impl Connection {
@@ -16,53 +14,53 @@ impl Connection {
             token,
             socket,
             is_open: true,
-            received: String::new(),
-            to_send: String::new(),
+            received: Vec::with_capacity(4096),
+            to_send: Vec::with_capacity(4096),
         }
     }
 
-    pub fn read(&mut self) {
-        loop {
-            let read = self.socket.read_to_string(&mut self.received);
+    // pub fn read(&mut self) {
+    //     loop {
+    //         let read = self.socket.read_to_string(&mut self.received);
 
-            match read {
-                Ok(0) => {
-                    println!("Connection closed. 0 bytes received.");
-                    self.is_open = false;
-                }
-                Ok(n) => {
-                    println!("Data received. {} bytes received.", n);
-                }
-                // @todo What's ref doing? Maybe a temp var?
-                Err(ref e) if would_block(e) => {
-                    println!("Failed to read. Would block: {}", e);
-                    break;
-                }
-                Err(e) => {
-                    println!("Failed to read: {}", e);
-                    break;
-                }
-            }
-        }
-    }
+    //         match read {
+    //             Ok(0) => {
+    //                 println!("Connection closed. 0 bytes received.");
+    //                 self.is_open = false;
+    //             }
+    //             Ok(n) => {
+    //                 println!("Data received. {} bytes received.", n);
+    //             }
+    //             // @todo What's ref doing? Maybe a temp var?
+    //             Err(ref e) if would_block(e) => {
+    //                 println!("Failed to read. Would block: {}", e);
+    //                 break;
+    //             }
+    //             Err(e) => {
+    //                 println!("Failed to read: {}", e);
+    //                 break;
+    //             }
+    //         }
+    //     }
+    // }
 
-    pub fn write(&mut self) {
-        match self.socket.write_all(self.to_send.as_bytes()) {
-            Ok(_) => (),
-            Err(_) => {
-                self.is_open = false;
-                return;
-            }
-        }
+    // pub fn write(&mut self) {
+    //     match self.socket.write_all(self.to_send.as_bytes()) {
+    //         Ok(_) => (),
+    //         Err(_) => {
+    //             self.is_open = false;
+    //             return;
+    //         }
+    //     }
 
-        self.to_send.clear();
-    }
+    //     self.to_send.clear();
+    // }
 
-    pub fn set(&mut self, value: &str) {
-        self.to_send.push_str(value);
-    }
+    // pub fn set(&mut self, value: &str) {
+    //     self.to_send.push_str(value);
+    // }
 }
 
-fn would_block(e: &std::io::Error) -> bool {
-    e.kind() == std::io::ErrorKind::WouldBlock
-}
+// fn would_block(e: &std::io::Error) -> bool {
+//     e.kind() == std::io::ErrorKind::WouldBlock
+// }
