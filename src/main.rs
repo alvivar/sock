9ffa0f3b2fe0c8cd69f84477e8ed81684c27e8c1
Andrés Villalobos <andresalvivar@gmail.com>
2021-06-16 00:28:40 -0600

    It seems to be a multithread something that works but also needs lots of polish and refactor

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
-use mio::event::Event;
 use mio::net::TcpListener;
-use mio::{Events, Interest, Poll, Registry, Token};
+use mio::{Events, Interest, Poll, Token};
 
 use std::collections::HashMap;
 use std::io::{self, Read, Write};
 use std::str::from_utf8;
+use std::sync::mpsc::channel;
+use std::sync::{Arc, Mutex};
 
 use connection::Connection;
 mod connection;
+
+mod pool;
+use pool::ThreadPool;
 
 use env_logger;
 
@@ -35,6 +39,117 @@ fn main() -> io::Result<()> {
     // Unique token for each incoming connection.
     let mut unique_token = Token(SERVER.0 + 1);
 
+    // The thread pool that handles IO loads in the connection on the connection.
+    let mut pool = ThreadPool::new(4);
+
+    let (pool_tx, pool_rx) = channel::<Connection>();
+    let pool_rx = Arc::new(Mutex::new(pool_rx));
+
+    let (ready_tx, ready_rx) = channel::<Connection>();
+
+    for _ in 0..pool.size() {
+        let pool_rx = pool_rx.clone();
+        let ready_tx = ready_tx.clone();
+        pool.submit(move || {
+            loop {
+                let mut connection = pool_rx.lock().unwrap().recv().unwrap();
+
+                // Reading!
+
+                println!("Reading!");
+
+                let mut received_data = vec![0; 4096];
+                let mut bytes_read = 0;
+
+                // We can (maybe) read from the connection.
+                loop {
+                    match connection.socket.read(&mut received_data[bytes_read..]) {
+                        Ok(0) => {
+                            // Reading 0 bytes means the other side has closed the
+                            // connection or is done writing, then so are we.
+                            connection.open = false;
+                            break;
+                        }
+                        Ok(n) => {
+                            bytes_read += n;
+                            if bytes_read == received_data.len() {
+                                received_data.resize(received_data.len() + 1024, 0);
+                            }
+                        }
+                        // Would block "errors" are the OS's way of saying that the
+                        // connection is not actually ready to perform this I/O operation.
+                        Err(ref err) if would_block(err) => break,
+                        Err(ref err) if interrupted(err) => continue,
+                        // Other errors we'll consider fatal.
+                        Err(err) => {
+                            // return Err(err);
+                        }
+                    }
+                }
+
+                if bytes_read != 0 {
+                    let received_data = &received_data[..bytes_read];
+                    if let Ok(str_buf) = from_utf8(received_data) {
+                        println!("Received data: {}", str_buf.trim_end());
+                    } else {
+                        println!("Received (none UTF-8) data: {:?}", received_data);
+                    }
+
+                    // We received data. This is a good place to parse the data and
+                    // respond accordingly.
+
+                    connection.to_send.append(&mut received_data.into());
+                    // registry.reregister(&mut connection.socket, event.token(), Interest::WRITABLE)?;
+                }
+
+                if !connection.open {
+                    println!("Connection closed");
+                }
+
+                // Writing!
+
+                if connection.to_send.len() > 0 {
+                    println!("Writing");
+
+                    // We can (maybe) write to the connection.
+                    match connection.socket.write(&connection.to_send) {
+                        // We want to write the entire `DATA` buffer in a single go. If we
+                        // write less we'll return a short write error (same as
+                        // `io::Write::write_all` does).
+                        // Ok(n) if n < connection.to_send.len() => {
+                        //     return Err(io::ErrorKind::WriteZero.into())
+                        // }
+                        Ok(_) => {
+                            // After we've written something we'll reregister the connection
+                            // to only respond to readable events, and clear the information
+                            // to send buffer.
+                            connection.to_send.clear();
+
+                            // registry.reregister(&mut connection.socket, event.token(), Interest::READABLE)?
+                            // @todo Here is a place to reregister the connection to READ probably.
+                        }
+                        // Would block "errors" are the OS's way of saying that the
+                        // connection is not actually ready to perform this I/O operation.
+                        Err(ref err) if would_block(err) => {}
+                        // Got interrupted (how rude!), we'll try again.
+                        Err(ref err) if interrupted(err) => {
+                            // return handle_connection_event(registry, connection, event)
+                        }
+                        // Other errors we'll consider fatal.
+                        Err(err) => {
+                            // return Err(err);
+                        }
+                    }
+                }
+
+                // Now, let's send the connection to reregister again.
+
+                ready_tx.send(connection).unwrap();
+            }
+        });
+    }
+
+    // Client detections.
     println!("You can connect to the server using `nc`:");
     println!(" $ nc 127.0.0.1 9000");
     println!("You'll see our welcome message and anything you type will be printed here.");
@@ -77,16 +192,51 @@ fn main() -> io::Result<()> {
                 },
                 token => {
                     // Maybe received an event for a TCP connection.
-                    let done = if let Some(connection) = connections.get_mut(&token) {
-                        handle_connection_event(poll.registry(), connection, event)?
-                    } else {
-                        // Sporadic events happen, we can safely ignore them.
-                        false
-                    };
-                    if done {
-                        connections.remove(&token);
+                    if let Some(connection) = connections.remove(&token) {
+                        // poll.registry(),
+                        // handle_connection_event(connection, event)?;
+
+                        if event.is_readable() || event.is_writable() {
+                            pool_tx.send(connection).unwrap();
+                        }
                     }
+
+                    // Sporadic events happen, we can safely ignore them.
                 }
+            }
+        }
+
+        // Let's reregister the connection for his purpose.
+
+        loop {
+            let try_connection = ready_rx.try_recv();
+            match try_connection {
+                Ok(connection) if !connection.open => {
+                    println!("token {} closed", connection.token.0);
+                }
+                Ok(mut connection) => {
+                    if connection.to_send.len() > 0 {
+                        println!("token {} has something to send", connection.token.0);
+                        poll.registry()
+                            .reregister(
+                                &mut connection.socket,
+                                connection.token,
+                                Interest::WRITABLE,
+                            )
+                            .unwrap();
+                    } else {
+                        println!("token {} can receive something", connection.token.0);
+                        poll.registry()
+                            .reregister(
+                                &mut connection.socket,
+                                connection.token,
+                                Interest::READABLE,
+                            )
+                            .unwrap();
+                    }
+                    connections.insert(connection.token, connection);
+                }
+                _ => break,
             }
         }
     }
@@ -98,94 +248,91 @@ fn next(current: &mut Token) -> Token {
     Token(next)
 }
 
-/// Returns `true` if the connection is done.
-fn handle_connection_event(
-    registry: &Registry,
-    connection: &mut Connection,
-    event: &Event,
-) -> io::Result<bool> {
-    if event.is_writable() && connection.to_send.len() > 0 {
-        println!("Writting");
+// /// Returns `true` if the connection is done.
+// fn handle_connection_event(connection: &mut Connection, event: &Event) -> io::Result<bool> {
+//     if event.is_writable() && connection.to_send.len() > 0 {
+//         println!("Writting");
 
-        // We can (maybe) write to the connection.
-        match connection.socket.write(&connection.to_send) {
-            // We want to write the entire `DATA` buffer in a single go. If we
-            // write less we'll return a short write error (same as
-            // `io::Write::write_all` does).
-            // Ok(n) if n < DATA.len() => return Err(io::ErrorKind::WriteZero.into()),
-            Ok(_) => {
-                // After we've written something we'll reregister the connection
-                // to only respond to readable events, and clear the information
-                // to send buffer.
-                connection.to_send.clear();
-                registry.reregister(&mut connection.socket, event.token(), Interest::READABLE)?
-            }
-            // Would block "errors" are the OS's way of saying that the
-            // connection is not actually ready to perform this I/O operation.
-            Err(ref err) if would_block(err) => {}
-            // Got interrupted (how rude!), we'll try again.
-            Err(ref err) if interrupted(err) => {
-                return handle_connection_event(registry, connection, event)
-            }
-            // Other errors we'll consider fatal.
-            Err(err) => return Err(err),
-        }
-    }
+//         // We can (maybe) write to the connection.
+//         match connection.socket.write(&connection.to_send) {
+//             // We want to write the entire `DATA` buffer in a single go. If we
+//             // write less we'll return a short write error (same as
+//             // `io::Write::write_all` does).
+//             Ok(n) if n < connection.to_send.len() => return Err(io::ErrorKind::WriteZero.into()),
+//             Ok(_) => {
+//                 // After we've written something we'll reregister the connection
+//                 // to only respond to readable events, and clear the information
+//                 // to send buffer.
+//                 connection.to_send.clear();
 
-    if event.is_readable() {
-        println!("Reading!");
+//                 // registry.reregister(&mut connection.socket, event.token(), Interest::READABLE)?
+//                 // @todo Here is a place to reregister the connection to READ probably.
+//             }
+//             // Would block "errors" are the OS's way of saying that the
+//             // connection is not actually ready to perform this I/O operation.
+//             Err(ref err) if would_block(err) => {}
+//             // Got interrupted (how rude!), we'll try again.
+//             Err(ref err) if interrupted(err) => {
+//                 // return handle_connection_event(connection, event);
+//             }
+//             // Other errors we'll consider fatal.
+//             Err(err) => return Err(err),
+//         }
+//     }
 
-        // let mut connection_closed = false;
-        let mut received_data = vec![0; 4096];
-        let mut bytes_read = 0;
+//     if event.is_readable() {
+//         println!("Reading!");
 
-        // We can (maybe) read from the connection.
-        loop {
-            match connection.socket.read(&mut received_data[bytes_read..]) {
-                Ok(0) => {
-                    // Reading 0 bytes means the other side has closed the
-                    // connection or is done writing, then so are we.
-                    connection.open = false;
-                    break;
-                }
-                Ok(n) => {
-                    bytes_read += n;
-                    if bytes_read == received_data.len() {
-                        received_data.resize(received_data.len() + 1024, 0);
-                    }
-                }
-                // Would block "errors" are the OS's way of saying that the
-                // connection is not actually ready to perform this I/O operation.
-                Err(ref err) if would_block(err) => break,
-                Err(ref err) if interrupted(err) => continue,
-                // Other errors we'll consider fatal.
-                Err(err) => return Err(err),
-            }
-        }
+//         let mut received_data = vec![0; 4096];
+//         let mut bytes_read = 0;
 
-        if bytes_read != 0 {
-            let received_data = &received_data[..bytes_read];
-            if let Ok(str_buf) = from_utf8(received_data) {
-                println!("Received data: {}", str_buf.trim_end());
-            } else {
-                println!("Received (none UTF-8) data: {:?}", received_data);
-            }
+//         // We can (maybe) read from the connection.
+//         loop {
+//             match connection.socket.read(&mut received_data[bytes_read..]) {
+//                 Ok(0) => {
+//                     // Reading 0 bytes means the other side has closed the
+//                     // connection or is done writing, then so are we.
+//                     connection.open = false;
+//                     break;
+//                 }
+//                 Ok(n) => {
+//                     bytes_read += n;
+//                     if bytes_read == received_data.len() {
+//                         received_data.resize(received_data.len() + 1024, 0);
+//                     }
+//                 }
+//                 // Would block "errors" are the OS's way of saying that the
+//                 // connection is not actually ready to perform this I/O operation.
+//                 Err(ref err) if would_block(err) => break,
+//                 Err(ref err) if interrupted(err) => continue,
+//                 // Other errors we'll consider fatal.
+//                 Err(err) => return Err(err),
+//             }
+//         }
 
-            // We received data. This is a good place to parse the data and
-            // respond accordingly.
+//         if bytes_read != 0 {
+//             let received_data = &received_data[..bytes_read];
+//             if let Ok(str_buf) = from_utf8(received_data) {
+//                 println!("Received data: {}", str_buf.trim_end());
+//             } else {
+//                 println!("Received (none UTF-8) data: {:?}", received_data);
+//             }
 
-            connection.to_send.append(&mut received_data.into());
-            registry.reregister(&mut connection.socket, event.token(), Interest::WRITABLE)?;
-        }
+//             // We received data. This is a good place to parse the data and
+//             // respond accordingly.
 
-        if !connection.open {
-            println!("Connection closed");
-            return Ok(true);
-        }
-    }
+//             connection.to_send.append(&mut received_data.into());
+//             // registry.reregister(&mut connection.socket, event.token(), Interest::WRITABLE)?;
+//         }
 
-    Ok(false)
-}
+//         if !connection.open {
+//             println!("Connection closed");
+//             return Ok(true);
+//         }
+//     }
+
+//     Ok(false)
+// }
 
 fn would_block(err: &io::Error) -> bool {
     err.kind() == io::ErrorKind::WouldBlock
